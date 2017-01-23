14b0e704f09341dff4d0825dd9c4b4d7b4f1bcf5
nehaghate <ghate.n@husky.neu.edu>
2017-01-23 17:15:57 -0500

    fixing travis

---
+++ src/main/java/parkspot/cloudcomp/App.java
@@ -0,0 +1,13 @@
+package parkspot.cloudcomp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
