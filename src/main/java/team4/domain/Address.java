9444eb923a74d084d8198bc92e0be56cd874fe4c
nehaghate <ghate.n@husky.neu.edu>
2017-02-01 10:19:39 -0500

    added usercontroller and address

---
+++ src/main/java/team4/domain/Address.java
@@ -0,0 +1,45 @@
+package team4.domain;
+
+public class Address {
+
+	private String streetName;
+	
+	private String city;
+	
+	private String state;
+	
+	private int zipCode;
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+}
