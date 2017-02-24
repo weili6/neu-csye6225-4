a1f4e18423b9025b3fc32b02ac1bbe9aa898430f
NehaGhateNEU <ghate.n@husky.neu.edu>
2017-02-23 20:02:35 -0500

    Create dynamodb script

---
+++ createDynamoTable.sh
@@ -0,0 +1,16 @@
+if [ $(aws dynamodb list-tables | grep csye6225) ]
+
+then 
+ echo "already exists" 
+ exit 0
+	fi 
+
+aws dynamodb create-table \
+    --table-name csye6225 \
+    --attribute-definitions \
+        AttributeName=personId,AttributeType=N \
+        AttributeName=userName,AttributeType=S \
+    --key-schema AttributeName=personId,KeyType=HASH AttributeName=userName,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
+
+
