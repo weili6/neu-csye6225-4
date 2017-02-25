e680d6c64524f4ef0621b048b3f3717181038b21
YalinLi0312 <li.yali@husky.neu.edu>
2017-02-25 01:44:18 -0500

    fix one json get error

--- setupWebSecurityGroup.sh
+++ setupWebSecurityGroup.sh
@@ -6,7 +6,7 @@ echo $groupname already exists, delete and re-create it.
 aws ec2 delete-security-group --group-name "$groupname"
 fi
 
-id=$(aws ec2 create-security-group --group-name "$groupname" --description "My web security group" --vpc-id vpc-946005f2| jq -r '.GroupId')
+id=$(aws ec2 create-security-group --group-name "$groupname" --description "My web security group" --vpc-id vpc-946005f2| jq -r '.SecurityGroups[0].GroupId')
 echo Groupid = $id
 
 aws ec2 authorize-security-group-ingress --group-id $id --protocol tcp --port 443 --cidr 0.0.0.0/0
