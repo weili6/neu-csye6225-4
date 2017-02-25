e4ec052180256555f5bd5c16975cb6568e144df6
Liren Huang <huang.l@husky.neu.edu>
2017-02-25 09:59:58 -0500

    minor change

--- createDynamoTable.sh
+++ createDynamoTable.sh
@@ -1,15 +1,14 @@
-if [ -n "$(aws dynamodb list-tables | grep csye6225)" ]
-
-then 
- echo "already exists" 
- exit 0
-	fi 
+#!/bin/bash
+if [ -n "$(aws dynamodb list-tables | grep csye6225)" ]; then 
+    echo "already exists" 
+    exit 0
+fi 
 
 aws dynamodb create-table \
     --table-name csye6225 \
     --attribute-definitions \
-        AttributeName=personId,AttributeType=N \
-        AttributeName=userName,AttributeType=S \
+    AttributeName=personId,AttributeType=N \
+    AttributeName=userName,AttributeType=S \
     --key-schema AttributeName=personId,KeyType=HASH AttributeName=userName,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
 
