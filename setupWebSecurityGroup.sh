8ff129e8f039221082d0dc388bd1b0a20646f8f0
Liren Huang <huang.l@husky.neu.edu>
2017-02-24 23:02:01 -0500

    web security group

---
+++ setupWebSecurityGroup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+groupname=db
+
+if [ -n "$(aws ec2 describe-security-groups --group-names "$groupname" 2>/dev/null)" ];then
+echo $groupname already exists, delete and re-create it.
+aws ec2 delete-security-group --group-name "$groupname"
+fi
+
+id=$(aws ec2 create-security-group --group-name "$groupname" --description "My web security group" | jq -r '.GroupId')
+echo Groupid = $id
+
+aws ec2 authorize-security-group-ingress --group-id $id --protocol tcp --port 443 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $id --protocol tcp --port 80 --cidr 0.0.0.0/0
