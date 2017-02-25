669198c325b9972453d011b404e0c3013d8dea5a
YalinLi0312 <li.yali@husky.neu.edu>
2017-02-25 00:16:07 -0500

    change some error

--- setupWebSecurityGroup.sh
+++ setupWebSecurityGroup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-groupname=db
+groupname=web
 
 if [ -n "$(aws ec2 describe-security-groups --group-names "$groupname" 2>/dev/null)" ];then
 echo $groupname already exists, delete and re-create it.
 aws ec2 delete-security-group --group-name "$groupname"
 fi
 
-id=$(aws ec2 create-security-group --group-name "$groupname" --description "My web security group" | jq -r '.GroupId')
+id=$(aws ec2 create-security-group --group-name "$groupname" --description "My web security group" --vpc-id vpc-946005f2| jq -r '.GroupId')
 echo Groupid = $id
 
 aws ec2 authorize-security-group-ingress --group-id $id --protocol tcp --port 443 --cidr 0.0.0.0/0
