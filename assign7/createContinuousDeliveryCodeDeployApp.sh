c55f2b7920b13bdc85366840707f5bc4c786f43d
Wei Li <li.wei6@husky.neu.edu>
2017-03-17 16:36:02 -0400

    update create app

--- assign7/createContinuousDeliveryCodeDeployApp.sh
+++ assign7/createContinuousDeliveryCodeDeployApp.sh
@@ -5,3 +5,12 @@
 
 APP_NAME="jobportal"
 aws deploy create-application --application-name "$APP_NAME"
+
+aws deploy create-deployment-group \
+    --application-name "$APP_NAME" \
+    --deployment-config-name CodeDeployDefault.OneAtATime \
+    --deployment-group-name "$APP_NAME" \
+    --deployment-style IN_PLACE \
+    --ec2-tag-filters Key=stack,Value=Production,Type=KEY_AND_VALUE \
+    --service-role-arn arn:aws:iam::277964780852:role/CodeDeployServiceRole
+    
