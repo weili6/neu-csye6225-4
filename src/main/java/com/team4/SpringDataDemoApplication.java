139703f82d142243f61e3fe7efe1384b3b59d2ab
Liren Huang <huang.l@husky.neu.edu>
2017-03-21 16:34:57 -0400

    config rds url

--- src/main/java/com/team4/SpringDataDemoApplication.java
+++ src/main/java/com/team4/SpringDataDemoApplication.java
@@ -2,18 +2,25 @@ package com.team4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.*" })
-public class SpringDataDemoApplication {
+public class SpringDataDemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringDataDemoApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringDataDemoApplication.class);
+    }
+	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
