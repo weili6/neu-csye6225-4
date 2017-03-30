ec3e23341934b5993bfa0d8d4c457c8cdf4f2980
YalinLi0312 <li.yali@husky.neu.edu>
2017-03-30 17:10:28 -0400

    Update AjaxController.java

--- src/main/java/com/team4/controller/AjaxController.java
+++ src/main/java/com/team4/controller/AjaxController.java
@@ -1,20 +1,18 @@
 package com.team4.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
